b27ffbbfa0ef28ff131f567001fdb1c6588af84d
massimoromanelli <romanelli.mass@gmail.com>
2026-07-16 09:27:57 -0400

    finish 1.9, get No feedback to show when total == 0

--- part1/unicafe/exercises/src/App.jsx
+++ part1/unicafe/exercises/src/App.jsx
@@ -2,62 +2,61 @@ import { useState } from 'react'
 
 const Title = ({title}) => {
   return(
-      <h1>
-        {title}
-      </h1>
+      <h1> {title} </h1>
   )
 }
 
 const Button = ({ onClick, text }) => {
   return (
-    <button onClick={onClick}>
-      {text}
-    </button>
+    <button onClick={onClick}> {text} </button>
   )
 }
 
 const Statistics = ({name, value}) => {
   if (name === "positive") {
     return(
-      <p>
-      {name} {value} %
-    </p>
+    <p> {name} {value} % </p>
     )
   }
 
   return(
-    <p>
-      {name} {value}
-    </p>
+    <p> {name} {value} </p>
   )
 }
 
 
 function App() {
-  const [good, setGood] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [neutral, setNeutral] = useState(0)
+const [clicks, setClicks] = useState({
+  good:0, bad:0, neutral:0
+})
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseBad = () => setBad(bad + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const total = good + bad + neutral
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positive = total === 0 ? 0 : (good / total) * 100
+  const increaseGood = () => setClicks({...clicks, good: clicks.good + 1})
+  const increaseBad = () => setClicks({...clicks, bad: clicks.bad + 1})
+  const increaseNeutral = () => setClicks({...clicks, neutral: clicks.neutral + 1})
+
+  const total = clicks.good + clicks.bad + clicks.neutral
+  const average = total === 0 ? 0 : (clicks.good - clicks.bad) / total
+  const positive = total === 0 ? 0 : (clicks.good / total) * 100
 
   return (
     <div>
-    <Title title="give feedback" />
-    <Button onClick={increaseGood} text='good' />
-    <Button onClick={increaseNeutral} text='neutral' />
-    <Button onClick={increaseBad} text='bad' />
-    <Title title="statistics"/>
-    <Statistics name="good" value={good} />
-    <Statistics name="neutral" value={neutral} />
-    <Statistics name="bad" value={bad} />
-    <Statistics name="all" value={total} />
-    <Statistics name="average" value={average} />
-    <Statistics name="positive" value={positive} />
+      <Title title="give feedback" />
+      <Button onClick={increaseGood} text="good" />
+      <Button onClick={increaseNeutral} text="neutral" />
+      <Button onClick={increaseBad} text="bad" />
+      <Title title="statistics" />
+      {total === 0 ? (
+        <p>No Feedback Given</p>
+      ) : (
+        <>
+        <Statistics name="good" value={clicks.good} />
+        <Statistics name="neutral" value={clicks.neutral} />
+        <Statistics name="bad" value={clicks.bad} />
+        <Statistics name="all" value={total} />
+        <Statistics name="average" value={average} />
+        <Statistics name="positive" value={positive} />
+        </>
+      )}
     </div>
   )
 }
